833cb508ffa3bcc3488086f15a2b3ca2a4df08df
lucifer0x17 <ayush.aa347@gmail.com>
2024-02-07 05:28:21 +0530

    feat: type env added

--- node/src/constants/vars.ts
+++ node/src/constants/vars.ts
@@ -8,18 +8,22 @@ export const NODE_WALLET_ADDRESS = process.env.NODE_WALLET_ADDRESS as string;
 export const WALLET_FILE = JSON.parse(
     readFileSync(path.join(__dirname, '../../wallet.json')).toString(),
 );
+export const PORT = process.env.PORT;
 
 
 
-export const validateEnvironmentVariables = (): void => {
+export const validateEnvironmentVariables = (): void | true => {
     const missingVariables = [];
 
     if (!NODE_WALLET_ADDRESS) missingVariables.push('NODE_WALLET_ADDRESS');
     if (!WALLET_FILE) missingVariables.push('WALLET_FILE');
+    if (!PORT) missingVariables.push('PORT');
 
     if (missingVariables.length > 0) {
         throw new Error(
             `Missing environment variables: ${missingVariables.join(', ')}`,
         );
+    } else {
+        return true
     }
 };
