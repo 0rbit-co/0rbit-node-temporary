9e8a2be454fd4ad6adee214c6c1513f608418aea
lucifer0x17 <ayush.aa347@gmail.com>
2024-02-07 04:47:22 +0530

    modify: restructured the whole repository

--- oracle/src/services/send.js
+++
@@ -1,8 +0,0 @@
-// this function will send a response to ao
-import { connect, createDataItemSigner } from '@permaweb/connect'
-
-const walletFile = process.env.WALLET_FILE || './wallet.json'
-
-export async function send(target, data, tags) {
-
-}
