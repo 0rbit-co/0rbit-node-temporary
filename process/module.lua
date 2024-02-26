7c1a0d6722081eba85fdbcb79253894ba5d58f7f
lucifer0x17 <ayush.aa347@gmail.com>
2024-02-26 07:07:11 +0530

    feat: module code added

---
+++ process/module.lua
@@ -0,0 +1,28 @@
+local orbit = { _version = "0.0.1", process = " WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s" }
+
+function orbit.get(url)
+    if type(url) ~= "string" then
+        return "Error: First argument must be a string"
+    end
+    Send({
+        Target = orbit.process,
+        Action = "Get-Real-Data",
+        Url = url
+    })
+end
+
+function orbit.post(url, body)
+    if type(url) ~= "string" then
+        return "Error: First argument must be a string"
+    end
+    if type(body) ~= "table" then
+        return "Error: Second argument must be a table"
+    end
+    local json = require("json")
+    Send({
+        Target = orbit.process,
+        Action = "Post-Real-Data",
+        Url = url,
+        Body = json.encode(body)
+    })
+end
