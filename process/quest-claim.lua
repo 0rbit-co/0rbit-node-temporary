34ebbb2b6ebc957154e17ec3c45e1f97e14d3e65
Ayush Agrawal <54178328+Lucifer0x17@users.noreply.github.com>
2024-05-21 20:21:29 +0530

    Update quest-claim.lua

--- process/quest-claim.lua
+++ process/quest-claim.lua
@@ -10,20 +10,27 @@ function AddClaim(Quest, Address, Owner, User)
     })
 end
 
+-- Handlers.add("Claim-Quest", Handlers.utils.hasMatchingTag("Action", "Claim"),
+--     function(M)
+--         assert(type(M.Quest) == 'string', 'Quest Name is required!')
+--         assert(type(M.User) == 'string', 'User Name is required!')
+--         if Utils.find(function(quest) return quest.Name == M.Quest end, quests) and
+--             not Utils.find(function(claim) return claim.Address == M.From end, Claims) then
+--             AddClaim(M.Quest, M.From, M.Owner, M.User)
+--             print('Claim Added')
+--             ao.send({ Target = M.From, Data = "Claim received." })
+--         else
+--             print('quest not found')
+--             ao.send({ Target = M.From, Data = Colors.red .. "Claim not found or already claimed!" .. Colors.reset })
+--         end
+--     end
+-- )
+
 Handlers.add("Claim-Quest", Handlers.utils.hasMatchingTag("Action", "Claim"),
     function(M)
-        assert(type(M.Quest) == 'string', 'Quest Name is required!')
-        assert(type(M.User) == 'string', 'User Name is required!')
-        if Utils.find(function(quest) return quest.Name == M.Quest end, quests) and
-            not Utils.find(function(claim) return claim.Address == M.From end, Claims) then
-            AddClaim(M.Quest, M.From, M.Owner, M.User)
-            print('Claim Added')
-            ao.send({ Target = M.From, Data = "Claim received." })
-        else
-            print('quest not found')
-            ao.send({ Target = M.From, Data = Colors.red .. "Claim not found or already claimed!" .. Colors.reset })
-        end
+        Handlers.utils.reply("Quests have been paused for some time.")
     end
 )
+
 
 Send({ Target = "O3SXXYqQCNTbBedJjsW6wkPnrKFZq8DPLkKjO7zhztE", Action = "Claim", Quest = "News-Bot", User = "test" })
